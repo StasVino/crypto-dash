199205eea1eede34dc95d7698b787a05b31e99ee
unknown <st13vv@gmail.com>
2025-10-01 21:38:14 +0300

    added limit selector

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import { useEffect, useState } from 'react';
 import CoingCard from './components/CoinCard';
+import LimitSelector from './components/LImitSelector';
 const API_URL = import.meta.env.VITE_API_URL;
 
 const App = () => {
   const [coins, setCoins] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [limit, setLimit] = useState(10);
 
   useEffect(() => {
     const fetchCoins = async () => {
       try {
         const res = await fetch(
-          `${API_URL}&ordermarket_cap_desc&per_page=10&page=1&sparkline=false`
+          `${API_URL}&ordermarket_cap_desc&per_page=${limit}&page=1&sparkline=false`
         );
         if (!res.ok) {
           throw new error('Faild to fetch coins');
@@ -27,13 +29,14 @@ const App = () => {
     };
 
     fetchCoins();
-  }, []);
+  }, [limit]);
 
   return (
     <div>
       <h1>h Crypto Dash</h1>
       {loading && <p>Loading...</p>}
       {error && <div className="error">{error}</div>}
+      <LimitSelector limit={limit} onLimitChange={setLimit} />
       {!loading && !error && (
         <main className="grid">
           {coins.map((coin) => (
